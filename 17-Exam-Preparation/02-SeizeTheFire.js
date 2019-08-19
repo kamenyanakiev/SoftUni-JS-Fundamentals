859ec4d5136a709dbaa1637e31bfcd1508389101
Kamen Yanakiev <kamenqnakiev@gmail.com>
2019-08-19 14:37:51 +0300

    Exam Preparation

---
+++ 17-Exam-Preparation/02-SeizeTheFire.js
@@ -0,0 +1,41 @@
+function seizeTheFire(array) {
+  const field = array[0];
+  let currentWater = Number(array[1]);
+  let fireCells = field.split('#');
+  let totalFire = 0;
+  let effort = 0;
+  let cells = [];
+  for (let i = 0; i < fireCells.length; i++) {
+    let [typeOfFire, valueOfFire] = fireCells[i].split(' = ');
+    valueOfFire = Number(valueOfFire);
+    if (typeOfFire === 'High' && valueOfFire >= 81 && valueOfFire <= 125) {
+      if (currentWater >= valueOfFire) {
+        currentWater -= valueOfFire;
+        cells.push(valueOfFire);
+        effort += (valueOfFire * 0.25);
+        totalFire += valueOfFire;
+      }
+    } else if (typeOfFire === 'Medium' && valueOfFire >= 51 && valueOfFire <= 80) {
+      if (currentWater >= valueOfFire) {
+        currentWater -= valueOfFire;
+        cells.push(valueOfFire);
+        effort += (valueOfFire * 0.25);
+        totalFire += valueOfFire;
+      }
+    } else if (typeOfFire === 'Low' && valueOfFire >= 1 && valueOfFire <= 50) {
+      if (currentWater >= valueOfFire) {
+        currentWater -= valueOfFire;
+        cells.push(valueOfFire);
+        effort += (valueOfFire * 0.25);
+        totalFire += valueOfFire;
+      }
+    }
+  }
+  console.log('Cells:');
+  cells.forEach(element => {
+    console.log(` - ${element}`);
+  });
+  console.log(`Effort: ${effort.toFixed(2)}`);
+  console.log(`Total Fire: ${totalFire}`);
+}
+seizeTheFire(['High = 89#Low = 28#Medium = 77#Low = 23', '1250']);
