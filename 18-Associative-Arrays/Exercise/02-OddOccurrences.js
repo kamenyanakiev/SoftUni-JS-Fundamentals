1b913a455bf8d6d76c0914034768d10dcb3cfd71
Kamen Yanakiev <kamenqnakiev@gmail.com>
2019-09-02 12:42:07 +0300

    Associative arrays exercise

--- 18-Associative-Arrays/Exercise/02-OddOccurrences.js
+++ 18-Associative-Arrays/Exercise/02-OddOccurrences.js
@@ -1,4 +1,20 @@
-function oddOccurrences(arrray) {
-  
+function oddOccurrences(input) {
+  let occurances = new Map();
+  let inputArray = input.toLowerCase().split(' ');
+  for (let i = 0; i < inputArray.length; i++) {
+    let currentElement = inputArray[i];
+    let repeated = 1;
+    if (occurances.has(currentElement)) {
+      repeated += occurances.get(currentElement);
+    }
+    occurances.set(currentElement, repeated);
+  }
+  let occuranceArray = [...occurances];
+  let fitleredOccurance = occuranceArray.filter(element => element[1] % 2 !== 0);
+  let result = [];
+  fitleredOccurance.forEach(element => {
+    result.push(element[0]);
+  })
+  console.log(result.join(' '));  
 }
-oddOccurrences();
+oddOccurrences('Java C# Php PHP Java PhP 3 C# 3 1 5 C#');
