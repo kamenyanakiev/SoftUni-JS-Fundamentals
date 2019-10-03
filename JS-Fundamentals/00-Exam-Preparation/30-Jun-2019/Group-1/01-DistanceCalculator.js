3265363c9d580f6e52d3fb596828aaaa31bfaf98
Kamen Yanakiev <kamenqnakiev@gmail.com>
2019-10-03 15:21:20 +0300

    Exam Preparation

---
+++ JS-Fundamentals/00-Exam-Preparation/30-Jun-2019/Group-1/01-DistanceCalculator.js
@@ -0,0 +1,6 @@
+function distanceCalculator(array) {
+    const stepsMade = Number(array.shift());
+    const lengthOfOneStep = Number(array.shift());
+    const distanceNeeded = Number(array.shift());
+}
+distanceCalculator(['100', '2', '2']);
