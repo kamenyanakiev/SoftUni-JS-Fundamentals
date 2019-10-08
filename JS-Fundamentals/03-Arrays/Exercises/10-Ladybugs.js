4a081a0aa912d57318a017034ff2bebc392bb088
Kamen Yanakiev <kamenqnakiev@gmail.com>
2019-10-08 11:31:17 +0300

    Arrays Exercises

--- JS-Fundamentals/03-Arrays/Exercises/10-Ladybugs.js
+++ JS-Fundamentals/03-Arrays/Exercises/10-Ladybugs.js
@@ -1,82 +1,47 @@
 function ladybugs(array) {
+    const fieldLength = Number(array.shift());
+    const ladybugsPlaces = array.shift().split(' ');
     let ladybugArray = [];
-    const arrayLength = Number(array.shift());
-    let initalLadybugs = array.shift().split(' ');
-    for (let i = 0; i < arrayLength; i++) {
-        if (i === Number(initalLadybugs[0])) {
+    let initialPosition;
+    for (let i = 0; i < fieldLength; i++) {
+        if (i === Number(ladybugsPlaces[0])) {
+            ladybugsPlaces.shift();
             ladybugArray.push(1);
-            initalLadybugs.shift();
         } else {
             ladybugArray.push(0);
         }
     }
     array.forEach(element => {
-        const [currentIndex, direction, flyLength] = element.split(' ');
-        if (ladybugArray[Number(currentIndex)] === 1) {
-            moveLadybug(Number(currentIndex), direction, Number(flyLength));
+        let [position, direction, steps] = element.split(' ');
+        position = Number(position);
+        if (direction === 'left') {
+            steps = steps * -1;
+        } else {
+            steps = Number(steps);
+        }
+
+        if (ladybugArray[position] === 1) {
+            if (position + steps < 0 || position + steps >= fieldLength) {
+                ladybugArray[position] = 0;
+            } else {
+                initialPosition = position;
+                fly(position, steps);
+            }
         }
     });
 
+    function fly(position, steps) {
+        let nextPosition = position + steps;
+        if (nextPosition < 0 || nextPosition >= ladybugArray.length) {
+            ladybugArray[initialPosition] = 0;
+        } else if (ladybugArray[nextPosition] === 0) {
+            ladybugArray[nextPosition] = 1;
+            ladybugArray[initialPosition] = 0;
+        } else {
+            fly(nextPosition, steps);
+        }
+    }
+
     console.log(ladybugArray.join(' '));
-
-    function moveLadybug(currentIndex, direction, flyLength) {
-        let newIndex = 0;
-        if (direction === 'left') {
-            newIndex = currentIndex - flyLength;
-            if (newIndex < 0) {
-                ladybugArray[currentIndex] = 0;
-            } else if (ladybugArray[newIndex] === 1) {
-                checkIfOccupied(direction, flyLength, currentIndex, newIndex);
-                if (newIndex < 0) {
-                    ladybugArray[currentIndex] = 0;
-                } else {
-                    ladybugArray[newIndex] = 1;
-                    ladybugArray[currentIndex] = 0;
-                }
-            }
-        } else if (direction === 'right') {
-            newIndex = currentIndex + flyLength;
-            if (newIndex > arrayLength - 1) {
-                ladybugArray[currentIndex] = 0;
-            } else if (ladybugArray[newIndex] === 1) {
-                checkIfOccupied(direction, flyLength, currentIndex, newIndex);
-                if (newIndex > arrayLength - 1) {
-                    ladybugArray[currentIndex] = 0;
-                } else {
-                    ladybugArray[newIndex] = 1;
-                    ladybugArray[currentIndex] = 0;
-                }
-            }
-        }
-    }
-
-    function checkIfOccupied(direction, flyLength, currentIndex, newIndex) {
-        let ladybugCanFly = true;
-        let fliesAway = false;
-        if (direction === 'left') {
-            if (ladybugArray[newIndex] === 1) {
-                newIndex--;
-                ladybugCanFly = false;
-            } else {
-                ladybugCanFly = true;
-            }
-            if (newIndex < 0) {
-                fliesAway = true;
-            }
-        } else if (direction === 'right') {
-            if (ladybugArray[newIndex] === 1) {
-                newIndex++;
-                ladybugCanFly = false;
-            } else {
-                ladybugCanFly = true;
-            }
-            if (newIndex > arrayLength - 1) {
-                fliesAway = true;
-            }
-        }
-        if (!ladybugCanFly && !fliesAway) {
-            checkIfOccupied(direction, flyLength, currentIndex, newIndex);
-        }
-    }
 }
 ladybugs([3, '0 1 2', '0 right 1', '1 right 1', '2 right 1']);
